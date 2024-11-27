3b30f559ae1a0366002c65e60712c648888915cd
ikorneychuk <advoco2005@gmail.com>
2024-11-27 13:33:49 +0300

    Update insert.js

--- js_algorithms/js/insert.js
+++ js_algorithms/js/insert.js
@@ -17,7 +17,7 @@ async function insert() {
         insert.innerHTML += `
         <li style="width: 210px" class="d-flex flex-column m-1 p-1 border bg-body">
         <h5 class="card-title">${content[key].name}</h5>
-        <img style="width: 180px" class="img-fluid align-self-center" src="${content[key].image"}>
+        <img style="width: 180px" class="img-fluid align-self-center" src="${content[key].image}">
         <p class="card-text">${content[key].invention}.</p>
         </li>
                 `
