f2d8543d490de1eb0ce6411a62d5257b8b21924c
ikorneychuk <advoco2005@gmail.com>
2024-11-27 11:26:54 +0300

    Add files via upload

---
+++ js_algorithms/js/insert.js
@@ -0,0 +1,27 @@
+async function insert() {
+    let file = await fetch("../data/inventors.json")
+    //console.log("responce:\n", responce, "\n /responce: \n")
+    let content = await file.text()
+    //console.log("await responce.text()\n", content)
+    content = JSON.parse(content)
+    content = content.slice(0, 12)
+    //console.log("content.slice(0, 9)", content)
+    let key
+    //for (key in content) {
+    //    console.log(content[key].id, content[key].title)
+    //    console.log(content[key])
+    //}
+
+    let insert = document.getElementById("insert")
+    for (key in content) {
+        insert.innerHTML += `
+        <li style="width: 210px" class="d-flex flex-column m-1 p-1 border bg-body">
+        <h5 class="card-title">${content[key].name}</h5>
+        <img style="width: 180px" class="align-self-center" src=${content[key].image}>
+        <p class="card-text">${content[key].invention}.</p>
+        </li>
+                `
+    }
+}
+
+insert()
