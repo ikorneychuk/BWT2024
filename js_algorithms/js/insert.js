6929c9b76f31fe0a0ab29cf4c82d570dd4867cf6
ikorneychuk <advoco2005@gmail.com>
2024-11-27 13:35:34 +0300

    Update insert.js

--- js_algorithms/js/insert.js
+++ js_algorithms/js/insert.js
@@ -1,17 +1,9 @@
 async function insert() {
     let file = await fetch("../../BWT2024/js_algorithms/data/inventors.json")
-    console.log("responce:\n", file, "\n /responce: \n")
     let content = await file.text()
-    console.log("await responce.text()\n", content)
     content = JSON.parse(content)
     content = content.slice(0, 12)
-    console.log("content.slice(0, 9)", content)
     let key
-    //for (key in content) {
-    //    console.log(content[key].id, content[key].title)
-    //    console.log(content[key])
-    //}
-
     let insert = document.getElementById("insert")
     for (key in content) {
         insert.innerHTML += `
